40d874d5fc66108f168a01e42a71097122051163
classstud123 <85238489+classstud123@users.noreply.github.com>
2021-10-19 21:14:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var canvas = new fabric.Canvas('myCanvas');
 function new_image()
 {
 	
-	fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+	fabric.Image.fromURL('wish.png', function(Img) {
 	block_image_object = Img;
 		
 	block_image_object.scaleToWidth(700);
